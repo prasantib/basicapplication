53db4ae22d9808a5d3cb1123efce99fb90431e51
prasantib <prasanti.babu@gmail.com>
2019-10-21 16:56:37 -0400

    mathtwo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args){
+        int mynumber;
+        mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+    }
+}
